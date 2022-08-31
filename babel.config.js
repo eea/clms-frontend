8761a4402b0715a4fe25ca19698bced6c47458c3
Mikel Larreategi <mlarreategi@codesyntax.com>
2022-08-31 15:41:25 +0200

    revert babel.config.js

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-//module.exports = require('@plone/volto/babel');
-const val = require('@plone/volto/babel');
-
-module.exports = function (api) {
-  let { plugins, presets } = val(api);
-  api.cache(true);
-  plugins.push('@babel/plugin-proposal-nullish-coalescing-operator');
-  return { plugins, presets };
-};
