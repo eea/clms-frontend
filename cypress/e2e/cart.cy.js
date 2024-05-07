936b46e728bc1e38a455f7e247577f04decabc25
Ion Lizarazu <ilizarazu@codesyntax.com>
2024-05-07 09:36:20 +0200

    cart cypress tests

--- cypress/e2e/cart.cy.js
+++ cypress/e2e/cart.cy.js
@@ -1,8 +1,12 @@
 import fsc2016p20med from '../fixtures/datasets/fsc2016p20med.json';
 import lc2019 from '../fixtures/datasets/lc2019.json';
+import wws2018 from '../fixtures/datasets/wws2018.json';
+import clc2018 from '../fixtures/datasets/clc2018.json';
 import pr from '../fixtures/products.json';
 import snow from '../fixtures/products/snow.json';
+import clc from '../fixtures/products/corine-land-cover.json';
 import gdlc from '../fixtures/products/global-dynamic-land-cover.json';
+import hrlwaw from '../fixtures/products/high-resolution-layer-water-and-wetness.json';
 import { setupBeforeEach, tearDownAfterEach } from '../support/e2e';
 
 const fsc2016p20med_cart = [
@@ -71,27 +75,7 @@ const lc2019_cart = [
   },
 ];
 
-const rest_cart = [
-  {
-    id: '0407d497d3c44bcd93ce8fd5bf78596a',
-    UID: '0407d497d3c44bcd93ce8fd5bf78596a',
-    unique_id: '0407d497d3c44bcd93ce8fd5bf78596a-1714027573333',
-    area: {
-      type: 'polygon',
-      value: [
-        -2.497558593753114,
-        43.143749567024514,
-        -1.2890625000034355,
-        42.64473395779312,
-      ],
-    },
-  },
-  {
-    id: '0407d497d3c44bcd93ce8fd5bf78596a',
-    UID: '0407d497d3c44bcd93ce8fd5bf78596a',
-    unique_id: '0407d497d3c44bcd93ce8fd5bf78596a-1714027590164',
-    area: { type: 'nuts', value: 'ES513', valueName: 'Lleida' },
-  },
+const wws2018_cart = [
   {
     id: '0acef4675a4c4a548896cca00281f434',
     UID: '0acef4675a4c4a548896cca00281f434',
@@ -112,33 +96,73 @@ const rest_cart = [
     unique_id: '0acef4675a4c4a548896cca00281f434-1714027690066',
     area: { type: 'nuts', value: 'NL12', valueName: 'Friesland (NL)' },
   },
-
-  {
-    UID: 'c987cc580fcc40f7b7b4c31fec0a5312',
-    file_id: 'c2451d85-073d-4ec4-aeae-be0e6da7a1eb',
-    area: 'Austria',
-    file: 'AT001L2_WIEN',
-    unique_id:
-      'c987cc580fcc40f7b7b4c31fec0a5312_c2451d85-073d-4ec4-aeae-be0e6da7a1eb',
-  },
-  {
-    UID: 'c987cc580fcc40f7b7b4c31fec0a5312',
-    file_id: '6ff9b55f-e0b8-47c5-bc45-325facf274c1',
-    area: 'Austria',
-    file: 'AT002L2_GRAZ',
-    unique_id:
-      'c987cc580fcc40f7b7b4c31fec0a5312_6ff9b55f-e0b8-47c5-bc45-325facf274c1',
-  },
 ];
 
-describe('Cart Tests', () => {
-  beforeEach(setupBeforeEach);
-  afterEach(tearDownAfterEach);
+const clc2018_cart = [
+  {
+    id: '0407d497d3c44bcd93ce8fd5bf78596a',
+    UID: '0407d497d3c44bcd93ce8fd5bf78596a',
+    unique_id: '0407d497d3c44bcd93ce8fd5bf78596a-1714027573333',
+    area: {
+      type: 'polygon',
+      value: [
+        -2.497558593753114,
+        43.143749567024514,
+        -1.2890625000034355,
+        42.64473395779312,
+      ],
+    },
+  },
+  {
+    id: '0407d497d3c44bcd93ce8fd5bf78596a',
+    UID: '0407d497d3c44bcd93ce8fd5bf78596a',
+    unique_id: '0407d497d3c44bcd93ce8fd5bf78596a-1714027590164',
+    area: { type: 'nuts', value: 'ES513', valueName: 'Lleida' },
+  },
+];
+// const rest_cart = [
+//   {
+//     UID: 'c987cc580fcc40f7b7b4c31fec0a5312',
+//     file_id: 'c2451d85-073d-4ec4-aeae-be0e6da7a1eb',
+//     area: 'Austria',
+//     file: 'AT001L2_WIEN',
+//     unique_id:
+//       'c987cc580fcc40f7b7b4c31fec0a5312_c2451d85-073d-4ec4-aeae-be0e6da7a1eb',
+//   },
+//   {
+//     UID: 'c987cc580fcc40f7b7b4c31fec0a5312',
+//     file_id: '6ff9b55f-e0b8-47c5-bc45-325facf274c1',
+//     area: 'Austria',
+//     file: 'AT002L2_GRAZ',
+//     unique_id:
+//       'c987cc580fcc40f7b7b4c31fec0a5312_6ff9b55f-e0b8-47c5-bc45-325facf274c1',
+//   },
+// ];
 
-  it('Check UTM projection selector:', () => {
+const success_reponse = {
+  statusCode: 200,
+  body: { msg: 'success!', status: 'success' },
+};
+
+describe('Cart Tests', () => {
+  beforeEach(() => {
+    cy.autologin();
     cy.clearLocalStorage();
     cy.intercept('GET', '@projections').as('projections');
     cy.intercept('GET', '@nuts_name').as('nuts_name');
+    cy.visit(`/en/cart`);
+    cy.wait('@projections');
+    cy.get('.ui.container h1').should('contain', 'Cart');
+    cy.get('.ui.container .ccl-container h2').should('contain', 'Empty cart');
+    cy.get('li a.header-login-link strong').should('contain', '0');
+  });
+  // afterEach(() => {
+  //   cy.clearLocalStorage();
+  // });
+
+  before(() => {
+    setupBeforeEach();
+    // create projects
     cy.createContentJSON({
       contentJSON: snow,
       path: `en/${pr.id}`,
@@ -147,17 +171,57 @@ describe('Cart Tests', () => {
     cy.setWorkflow({ path: `/en/${pr.id}/${snow.id}` });
 
     cy.createContentJSON({
+      contentJSON: gdlc,
+      path: `en/${pr.id}`,
+      extras: { mapviewer_component: gdlc.mapviewer_component },
+    });
+    cy.setWorkflow({ path: `/en/${pr.id}/${gdlc.id}` });
+
+    cy.createContentJSON({
+      contentJSON: hrlwaw,
+      path: `en/${pr.id}`,
+      extras: { mapviewer_component: hrlwaw.mapviewer_component },
+    });
+    cy.setWorkflow({ path: `/en/${pr.id}/${hrlwaw.id}` });
+
+    cy.createContentJSON({
+      contentJSON: clc,
+      path: `en/${pr.id}`,
+      extras: { mapviewer_component: clc.mapviewer_component },
+    });
+    cy.setWorkflow({ path: `/en/${pr.id}/${clc.id}` });
+
+    //create datasets
+    cy.createContentJSON({
       contentJSON: fsc2016p20med,
       path: `/en/${pr.id}/${snow.id}`,
     });
     cy.setWorkflow({ path: `/en/${pr.id}/${snow.id}/${fsc2016p20med.id}` });
 
-    cy.navigate(`/en/cart`);
-    cy.wait('@projections');
-    cy.get('.ui.container h1').should('contain', 'Cart');
-    cy.get('.ui.container .ccl-container h2').should('contain', 'Empty cart');
-    cy.get('li a.header-login-link strong').should('contain', '0');
+    cy.createContentJSON({
+      contentJSON: { ...lc2019, datasets: [] }, //ignore related datasets
+      path: `en/${pr.id}/${gdlc.id}`,
+    });
+    cy.setWorkflow({ path: `/en/${pr.id}/${gdlc.id}/${lc2019.id}` });
 
+    cy.createContentJSON({
+      contentJSON: { ...wws2018, datasets: [] }, //ignore related datasets
+      path: `en/${pr.id}/${hrlwaw.id}`,
+    });
+    cy.setWorkflow({ path: `/en/${pr.id}/${hrlwaw.id}/${wws2018.id}` });
+
+    cy.createContentJSON({
+      contentJSON: { ...clc2018, datasets: [] }, //ignore related datasets
+      path: `en/${pr.id}/${clc.id}`,
+    });
+    cy.setWorkflow({ path: `/en/${pr.id}/${clc.id}/${clc2018.id}` });
+  });
+
+  after(() => {
+    tearDownAfterEach();
+  });
+
+  it('Test UTM projection selector', () => {
     cy.visit(`/en/cart`, {
       onBeforeLoad(win) {
         win.localStorage.setItem(
@@ -166,8 +230,8 @@ describe('Cart Tests', () => {
         );
       },
     });
-    cy.wait('@projections');
     cy.get('li a.header-login-link strong').should('contain', '4');
+    cy.wait('@projections');
 
     cy.wait(2000);
     // first cart item check and modify the selection
@@ -226,7 +290,7 @@ describe('Cart Tests', () => {
 
     // Duplicate the last element
     cy.get('td.text-end').eq(3).find('.info-icon').eq(0).find('button').click();
-    cy.wait(500);
+    cy.wait(1000);
     cy.get('li a.header-login-link strong').should('contain', '5');
     cy.get('td.table-td-projections')
       .eq(4)
@@ -242,7 +306,7 @@ describe('Cart Tests', () => {
 
     // Duplicate the second element
     cy.get('td.text-end').eq(1).find('.info-icon').eq(0).find('button').click();
-    cy.wait(500);
+    cy.wait(1000);
     cy.get('li a.header-login-link strong').should('contain', '6');
     cy.get('td.table-td-projections')
       .eq(2)
@@ -262,7 +326,8 @@ describe('Cart Tests', () => {
       expect(req.body.Datasets[3].OutputGCS).to.eq('EPSG:3035');
       expect(req.body.Datasets[4].OutputGCS).to.eq('EPSG:32632');
       expect(req.body.Datasets[5].OutputGCS).to.eq('EPSG:32632');
-    });
+      req.reply(success_reponse);
+    }).as('datarequest_post');
 
     // Select entire cart
     cy.get('td.table-td-checkbox div.ui.checkbox input').each(($checkbox) => {
@@ -273,31 +338,16 @@ describe('Cart Tests', () => {
 
     // Download cart
     cy.get('a.ccl-button.ccl-button--default').click();
-  });
 
-  it('Check Timeseries calendar:', () => {
-    cy.clearLocalStorage();
-    cy.intercept('GET', '@projections').as('projections');
-    cy.intercept('GET', '@nuts_name').as('nuts_name');
-    cy.createContentJSON({
-      contentJSON: snow,
-      path: `en/${pr.id}`,
-      extras: { mapviewer_component: snow.mapviewer_component },
-    });
-    cy.setWorkflow({ path: `/en/${pr.id}/${snow.id}` });
-
-    cy.createContentJSON({
-      contentJSON: fsc2016p20med,
-      path: `/en/${pr.id}/${snow.id}`,
-    });
-    cy.setWorkflow({ path: `/en/${pr.id}/${snow.id}/${fsc2016p20med.id}` });
-
-    cy.navigate(`/en/cart`);
-    cy.wait('@projections');
+    //expect an empty cart
+    cy.wait('@datarequest_post');
+    cy.visit('/en/cart');
     cy.get('.ui.container h1').should('contain', 'Cart');
     cy.get('.ui.container .ccl-container h2').should('contain', 'Empty cart');
     cy.get('li a.header-login-link strong').should('contain', '0');
+  });
 
+  it('Test Timeseries calendar', () => {
     cy.visit(`/en/cart`, {
       onBeforeLoad(win) {
         win.localStorage.setItem(
@@ -375,10 +425,6 @@ describe('Cart Tests', () => {
         method: 'POST',
         url: '@datarequest_post',
       },
-      {
-        statusCode: 200,
-        body: { msg: 'success!', status: 'success' },
-      },
       (req) => {
         expect(req.body.Datasets[0].TemporalFilter.EndDate).to.be.greaterThan(
           0,
@@ -386,6 +432,7 @@ describe('Cart Tests', () => {
         expect(req.body.Datasets[0].TemporalFilter.StartDate).to.be.greaterThan(
           0,
         );
+        req.reply(success_reponse);
       },
     ).as('datarequest_post');
 
@@ -400,34 +447,14 @@ describe('Cart Tests', () => {
     cy.get('a.ccl-button.ccl-button--default').click();
 
     cy.wait('@datarequest_post');
+    cy.visit('/en/cart');
+
     cy.get('.ui.container h1').should('contain', 'Cart');
     cy.get('.ui.container .ccl-container h2').should('contain', 'Empty cart');
     cy.get('li a.header-login-link strong').should('contain', '0');
   });
 
-  it('Check Land Cover 2019:', () => {
-    cy.clearLocalStorage();
-    cy.intercept('GET', '@projections').as('projections');
-    cy.intercept('GET', '@nuts_name').as('nuts_name');
-    cy.createContentJSON({
-      contentJSON: gdlc,
-      path: `en/${pr.id}`,
-      extras: { mapviewer_component: gdlc.mapviewer_component },
-    });
-    cy.setWorkflow({ path: `/en/${pr.id}/${gdlc.id}` });
-
-    cy.createContentJSON({
-      contentJSON: { ...lc2019, datasets: [] }, //ignore related datasets
-      path: `en/${pr.id}/${gdlc.id}`,
-    });
-    cy.setWorkflow({ path: `/en/${pr.id}/${gdlc.id}/${lc2019.id}` });
-
-    cy.navigate(`/en/cart`);
-    cy.wait('@projections');
-    cy.get('.ui.container h1').should('contain', 'Cart');
-    cy.get('.ui.container .ccl-container h2').should('contain', 'Empty cart');
-    cy.get('li a.header-login-link strong').should('contain', '0');
-
+  it('Test Layer/Band selector', () => {
     cy.visit(`/en/cart`, {
       onBeforeLoad(win) {
         win.localStorage.setItem(
@@ -439,6 +466,7 @@ describe('Cart Tests', () => {
     cy.wait('@projections');
     cy.get('li a.header-login-link strong').should('contain', '2');
     cy.wait(2000);
+
     // first cart item check and modify the selection
     cy.get('td.table-td-projections')
       .eq(0)
@@ -475,10 +503,6 @@ describe('Cart Tests', () => {
         method: 'POST',
         url: '@datarequest_post',
       },
-      {
-        statusCode: 200,
-        body: { msg: 'success!', status: 'success' },
-      },
       (req) => {
         expect(req.body.Datasets[0].OutputGCS).to.eq('EPSG:3035');
         expect(req.body.Datasets[0].Layer).to.eq('Cover Fraction: Cropland');
@@ -486,6 +510,7 @@ describe('Cart Tests', () => {
         expect(req.body.Datasets[1].Layer).to.eq(
           'Land Cover Classification: Discrete classification',
         );
+        req.reply(success_reponse);
       },
     ).as('datarequest_post');
 
@@ -499,6 +524,165 @@ describe('Cart Tests', () => {
     // Download cart
     cy.get('a.ccl-button.ccl-button--default').click();
     cy.wait('@datarequest_post');
+    cy.visit('/en/cart');
+
+    cy.get('.ui.container h1').should('contain', 'Cart');
+    cy.get('.ui.container .ccl-container h2').should('contain', 'Empty cart');
+    cy.get('li a.header-login-link strong').should('contain', '0');
+  });
+
+  it('Test Collection selector', () => {
+    cy.visit(`/en/cart`, {
+      onBeforeLoad(win) {
+        win.localStorage.setItem(
+          'cart_session_admin',
+          JSON.stringify(wws2018_cart),
+        );
+      },
+    });
+    cy.wait('@projections');
+    cy.get('li a.header-login-link strong').should('contain', '2');
+    cy.wait(2000);
+
+    // first cart item check and modify the selectors
+    cy.get('td.table-td-projections')
+      .eq(0)
+      .then(($line) => {
+        let selected = $line.find('.ui.selection.dropdown div.text').eq(0);
+        const choices = $line.find('.ui.selection.dropdown .item');
+        expect(selected.text()).to.eq(
+          'EPSG:3035 (Source system of the dataset)',
+        );
+        expect(choices).to.have.lengthOf(4);
+        selected.click();
+        choices.eq(1).click();
+        choices.eq(1).click();
+        selected = $line.find('.ui.selection.dropdown div.text').eq(0);
+        expect(selected.text()).to.eq('EPSG:3857');
+      });
+    cy.get('td .ui.selection.dropdown.collection-selector')
+      .eq(0)
+      .then(($selector) => {
+        let selected = $selector.find('div.divider.text');
+        const choices = $selector.find('div.menu .item');
+        expect(selected.text()).to.eq('10 m');
+        expect(choices).to.have.lengthOf(2);
+        choices.eq(1).click();
+        choices.eq(1).click();
+        expect(selected.text()).to.eq('100 m');
+      });
+
+    // intercept the POST and check the body data
+    cy.intercept(
+      {
+        method: 'POST',
+        url: '@datarequest_post',
+      },
+      (req) => {
+        expect(req.body.Datasets[0].OutputGCS).to.eq('EPSG:3857');
+        expect(req.body.Datasets[0].DatasetDownloadInformationID).to.eq(
+          '95edd973-a059-46f1-b5dd-6fb2287dbb86',
+        );
+
+        expect(req.body.Datasets[1].OutputGCS).to.eq('EPSG:3035');
+        expect(req.body.Datasets[1].DatasetDownloadInformationID).to.eq(
+          'd170eb21-c4b4-47f5-88c7-958359704542',
+        );
+        req.reply(success_reponse);
+      },
+    ).as('datarequest_post');
+
+    // Select entire cart
+    cy.get('td.table-td-checkbox div.ui.checkbox input').each(($checkbox) => {
+      expect($checkbox.parent()).to.not.have.class('checked');
+      $checkbox.click();
+      expect($checkbox.parent()).to.have.class('checked');
+    });
+
+    // Download cart
+    cy.get('a.ccl-button.ccl-button--default').click();
+    cy.wait('@datarequest_post');
+    cy.visit('/en/cart');
+
+    cy.get('.ui.container h1').should('contain', 'Cart');
+    cy.get('.ui.container .ccl-container h2').should('contain', 'Empty cart');
+    cy.get('li a.header-login-link strong').should('contain', '0');
+  });
+
+  it('Test Type and Format selectors', () => {
+    cy.visit(`/en/cart`, {
+      onBeforeLoad(win) {
+        win.localStorage.setItem(
+          'cart_session_admin',
+          JSON.stringify(clc2018_cart),
+        );
+      },
+    });
+
+    cy.wait('@projections');
+    cy.get('li a.header-login-link strong').should('contain', '2');
+    cy.wait(2000);
+
+    cy.get('td .collection-container').eq(0).should('have.text', '-');
+    cy.get('td .ui.selection.dropdown.format-selector')
+      .eq(0)
+      .then(($selector) => {
+        let selected = $selector.find('div.divider.text');
+        const choices = $selector.find('div.menu .item');
+        expect(selected.text()).to.eq('File Geodatabase (FGDB)');
+        expect(choices).to.have.lengthOf(5);
+        choices.eq(1).click();
+        choices.eq(1).click();
+        expect(selected.text()).to.eq('Geography Markup Language (GML)');
+      });
+    cy.get('td .ui.selection.dropdown.type-selector')
+      .eq(0)
+      .then(($selector) => {
+        let selected = $selector.find('div.divider.text');
+        const choices = $selector.find('div.menu .item');
+        expect(selected.text()).to.eq('VECTOR');
+        expect(choices).to.have.lengthOf(2);
+        choices.eq(1).click();
+        choices.eq(1).click();
+        expect(selected.text()).to.eq('RASTER');
+      });
+    cy.get('td .collection-container').eq(0).should('have.text', '100 m');
+    cy.get('td .format-container').eq(0).should('have.text', 'Geotiff');
+
+    // intercept the POST and check the body data
+    cy.intercept(
+      {
+        method: 'POST',
+        url: '@datarequest_post',
+      },
+      (req) => {
+        expect(req.body.Datasets[0].OutputGCS).to.eq('EPSG:3035');
+        expect(req.body.Datasets[0].OutputFormat).to.eq('Geotiff');
+        expect(req.body.Datasets[0].DatasetDownloadInformationID).to.eq(
+          '7bcdf9d1-6ba0-4d4e-afa8-01451c7316cb',
+        );
+
+        expect(req.body.Datasets[1].OutputGCS).to.eq('EPSG:3035');
+        expect(req.body.Datasets[1].OutputFormat).to.eq('GDB');
+        expect(req.body.Datasets[1].DatasetDownloadInformationID).to.eq(
+          '1bda2fbd-3230-42ba-98cf-69c96ac063bc',
+        );
+        req.reply(success_reponse);
+      },
+    ).as('datarequest_post');
+
+    // Select entire cart
+    cy.get('td.table-td-checkbox div.ui.checkbox input').each(($checkbox) => {
+      expect($checkbox.parent()).to.not.have.class('checked');
+      $checkbox.click();
+      expect($checkbox.parent()).to.have.class('checked');
+    });
+
+    // Download cart
+    cy.get('a.ccl-button.ccl-button--default').click();
+    cy.wait('@datarequest_post');
+    cy.visit('/en/cart');
+
     cy.get('.ui.container h1').should('contain', 'Cart');
     cy.get('.ui.container .ccl-container h2').should('contain', 'Empty cart');
     cy.get('li a.header-login-link strong').should('contain', '0');
